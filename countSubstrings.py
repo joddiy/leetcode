863200e83f25a6601d4572b6ecf91a2cac33b191
joddiy <joddiy@qq.com>
2018-12-12 15:12:40 +0800

    m

--- countSubstrings.py
+++ countSubstrings.py
@@ -5,9 +5,28 @@
 # ------------------------------------------------------------------------
 
 class Solution(object):
+
     def countSubstrings(self, s):
         """
         :type s: str
         :rtype: int
         """
+        self.count = 0
 
+        def extendPalindrome(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                self.count += 1
+                left -= 1
+                right += 1
+
+        if s is None or len(s) == 0:
+            return 0
+
+        for i in range(len(s)):
+            extendPalindrome(s, i, i)
+            extendPalindrome(s, i, i + 1)
+
+        return self.count
+
+
+print(Solution().countSubstrings("abc"))
---
+++ topKFrequent.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# file: topKFrequent.py
+# author: joddiyzhang@gmail.com
+# time: 2018/11/26 11:55 AM
+# ------------------------------------------------------------------------
+
