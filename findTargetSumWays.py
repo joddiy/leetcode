cbeb7d8740c2234e5003738eb8df0ea3d53da8ba
joddiy <joddiy@qq.com>
2019-05-17 13:52:01 +0800

    add

---
+++ findTargetSumWays.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        
+
+
+Solution().findTargetSumWays([1, 1, 1, 1, 1], 3)
