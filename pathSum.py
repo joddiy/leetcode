44a86d7d2c68e3a1f390abba625ea3f43823a923
joddiy <joddiy@qq.com>
2019-05-20 23:04:47 +0800

    add

---
+++ pathSum.py
@@ -0,0 +1,14 @@
+from utils.tools import treeNodeToString, TreeNode, stringToTreeNode
+
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        return sum
+
+
+Solution().pathSum(stringToTreeNode("[10,5,-3,3,2,null,11,3,-2,null,1]"), 8)
