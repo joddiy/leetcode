079d60df09368aa560303f40e89e0a0883b22ec3
joddiy <joddiy@qq.com>
2019-05-21 01:05:37 +0800

    add

---
+++ maxSubArray.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_so_far = 0
+        max_ending_here = 0
+        for i in range(len(nums)):
+            max_ending_here += nums[i]
+            if max_ending_here < 0:
+                max_ending_here = 0
+            elif max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+        if max_so_far == 0:
+            return max(nums)
+        return max_so_far
+
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = nums[0]
+        for i in range(1, len(nums)):
+            nums[i] = max(0, nums[i-1]) + nums[i]
+            ret = max(ret, nums[i])
+        return ret
+
+
+Solution().maxSubArray([-1,1])
