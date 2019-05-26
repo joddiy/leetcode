0209bd350c86eb275e7743785c72c291fc3f3448
joddiy <joddiy@qq.com>
2019-05-27 01:55:43 +0800

    add

--- canPartition.py
+++ canPartition.py
@@ -31,6 +31,23 @@ class Solution(object):
                         return True
         return False
 
+    def canFindSum(self, nums, target, ind, n, d):
+        if target in d: return d[target] 
+        if target == 0: d[target] = True
+        else:
+            d[target] = False
+            if target > 0:
+                for i in xrange(ind, n):
+                    if self.canFindSum(nums, target - nums[i], i+1, n, d):
+                        d[target] = True
+                        break
+        return d[target]
+    
+    def canPartition(self, nums):
+        s = sum(nums)
+        if s % 2 != 0: return False
+        return self.canFindSum(nums, s/2, 0, len(nums), {})
+
 
 Solution().canPartition(
 [66,90,7,6,32,16,2,78,69,88,85,26,3,9,58,65,30,96,11,31,99,49,63,83,79,97,20,64,81,80,25,69,9,75,23,70,26,71,25,54,1,40,41,82,32,10,26,33,50,71,5,91,59,96,9,15,46,70,26,32,49,35,80,21,34,95,51,66,17,71,28,88,46,21,31,71,42,2,98,96,40,65,92,43,68,14,98,38,13,77,14,13,60,79,52,46,9,13,25,8])
