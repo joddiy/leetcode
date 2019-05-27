c27a74c2856461db7c320f0d1a964ffff632b71f
joddiy <joddiy@qq.com>
2019-05-27 18:09:10 +0800

    add

---
+++ mergeKLists.py
@@ -0,0 +1,27 @@
+from utils.tools import *
+import heapq
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = []
+        for l in lists:
+            if l:
+                heapq.heappush(q, (l.val, l))
+        while not q:
+            val, node = heapq.heappop(q)
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                heapq.heappush((q, (node.val, node)))
+        return head.next
+
+
+Solution().mergeKLists([stringToListNode('[1,4,5]'), stringToListNode(
+    '[1,3,4]'), stringToListNode('[2,6]')])
