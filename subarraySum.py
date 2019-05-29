a162555c22d2b8f6611a20e39e1a87840ae43f81
joddiy <joddiy@qq.com>
2019-05-29 20:44:41 +0800

    add

--- subarraySum.py
+++ subarraySum.py
@@ -1,3 +1,6 @@
+from utils.tools import *
+
+
 class Solution(object):
     def subarraySum(self, nums, k):
         """
@@ -5,19 +8,35 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        s = sum(nums)
-        n, m = len(nums), 2 * k + 1
+        count = 0
+        n = len(nums)
+        for i in range(0, n):
+            sum = 0
+            for j in range(i, n):
+                sum += nums[j]
+                if sum == k:
+                    count += 1
+        return count
 
-        dp = [[0, 0] * m for _ in range(n + 1)]
-        dp[0][nums[0]] = 1
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        hash_map = {0: 1}
+        curr_sum = 0
+        ret = 0
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            offset = curr_sum - k
+            if offset in hash_map:
+                ret += hash_map[offset]
+            if curr_sum in hash_map:
+                hash_map[curr_sum] += 1
+            else:
+                hash_map[curr_sum] = 1
+        return ret
 
-        for i in range(n):
-            for j in range(nums[i], m - nums[i]):
-                if dp[i][j]:
-                    dp[i + 1][j - nums[i]] += dp[i][j]
-                    dp[i + 1][j] += dp[i][j]
 
-        print(dp)
-        return dp[-1][s + k]
-
-Solution().subarraySum([1, 1, 1], 2)
+Solution().subarraySum([3, 4, 7, 2, -3, 1, 4, 2], 7)
