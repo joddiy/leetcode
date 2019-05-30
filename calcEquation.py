2f5cfa9bafef930a8b34573edd2cf84944496408
joddiy <joddiy@qq.com>
2019-05-30 17:00:57 +0800

    add

--- calcEquation.py
+++ calcEquation.py
@@ -12,10 +12,13 @@ class Solution(object):
             quot[num][num] = quot[den][den] = 1.0
             quot[num][den] = val
             quot[den][num] = 1 / val
-        
         # Floyd–Warshall algorithm
-        for k in quot:
+        for k in quot:  # k is entire set
             for i in quot[k]:
                 for j in quot[k]:
                     quot[i][j] = quot[i][k] * quot[k][j]
         return [quot[num].get(den, -1.0) for num, den in queries]
+
+
+Solution().calcEquation([["a", "b"], ["b", "c"]], [2.0, 3.0], [
+    ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]])
