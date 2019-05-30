db3c2afad42d2c260310bbf151d0a3d272da75cd
joddiy <joddiy@qq.com>
2019-05-30 16:25:18 +0800

    add

--- calcEquation.py
+++ calcEquation.py
@@ -6,20 +6,14 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
-        i = 0
-        char_set = set([char for item in equations for char in item])
-        map_hash = dict((char, i) for i, char in enumerate(char_set))
-        print(map_hash)
-        matrix = [[0 for i in range(len(map_hash))] for j in range(len(map_hash))]
-        print(matrix)
-        for id, item in enumerate(equations):
-            matrix[map_hash[item[0]]][map_hash[item[1]]] = values[id]
-            matrix[map_hash[item[1]]][map_hash[item[0]]] = 1/values[id]
-        print(matrix)
-        # for char_x in range(len(map_hash)):
-            # for char_y in range(len(map_hash)):
-                # matrix[char_x][char_y] = 0
-
-
-Solution().calcEquation([["a", "b"], ["b", "c"]], [2.0, 3.0], [
-    ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]])
+        import collections
+        quot = collections.defaultdict(dict)
+        for (num, den), val in zip(equations, values):
+            quot[num][num] = quot[den][den] = 1.0
+            quot[num][den] = val
+            quot[den][num] = 1 / val
+        for k in quot:
+            for i in quot[k]:
+                for j in quot[k]:
+                    quot[i][j] = quot[i][k] * quot[k][j]
+        return [quot[num].get(den, -1.0) for num, den in queries]
