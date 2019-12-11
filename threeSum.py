97ad74d8869e66f94206562d7dffb1c555602803
joddiy <joddiy@qq.com>
2019-12-11 17:52:29 +0800

    commit

--- threeSum.py
+++ threeSum.py
@@ -1,4 +1,6 @@
 class Solution(object):
+
+    # O(n^2)
     def threeSum(self, nums):
         """
         :type nums: List[int]
@@ -10,7 +12,7 @@ class Solution(object):
         n = len(nums)
         for i in range(n):
             k = n-1
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:  # avoid repeat
                 continue
             for j in range(i+1, n):
                 if k <= j:
@@ -26,8 +28,29 @@ class Solution(object):
                         k -= 1
         return ret
 
-
+     # same idea, O(n^2)
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
 
 
 Solution().threeSum([-2, 0, 0, 2, 2])
-
