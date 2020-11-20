32759167641c8da0ba53660d159367ab50f8bb32
joddiy <joddiy@qq.com>
2020-11-20 18:02:01 +0800

    f

--- problems/maxSlidingWindow.py
+++ problems/maxSlidingWindow.py
@@ -1,4 +1,5 @@
 from tools import *
+import collections
 
 
 class Solution(object):
@@ -20,6 +21,56 @@ class Solution(object):
                 max_ = max(nums[i + 1 - k:i + 1])
             ret.append(max_)
         return ret
+
+    @print_
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        queue = collections.deque()
+        n = len(nums)
+        pt = 0
+        res = []
+        while pt < n:
+            # remove all numbers out of range
+            while queue and pt - queue[0] > k - 1:
+                queue.popleft()
+            # maintain descending order in queue
+            while queue and nums[queue[-1]] < nums[pt]:
+                queue.pop()
+
+            queue.append(pt)
+            if pt >= k - 1:
+                res.append(nums[queue[0]])
+            pt += 1
+        return res
+
+    @print_
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # Using deque, O(n)
+        deq = collections.deque([k - 1])  # Store indices
+        for i in range(k - 2, -1, -1):
+            if nums[i] > nums[deq[0]]:
+                deq.appendleft(i)
+
+        ans = [nums[deq[0]]]
+        for i in range(k, len(nums)):
+            if deq[0] == i - k:
+                deq.popleft()
+            while deq and nums[i] >= nums[deq[-1]]:
+                deq.pop()
+
+            deq.append(i)
+            ans.append(nums[deq[0]])
+
+        return ans
 
 
 solution = Solution().maxSlidingWindow
