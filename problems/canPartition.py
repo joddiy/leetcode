bc3ab6fc5f6560f0d7de87052481905da9ccbc7b
Zhaoqi Zhang <joddiyzhang@gmail.com>
2022-10-05 14:55:36 +0800

    commit

--- problems/canPartition.py
+++ problems/canPartition.py
@@ -1,46 +1,59 @@
-def solution(nums):
-    sum_v = sum(nums)
-    if sum_v % 2 == 1:
-        return False
-    if max(nums) > sum_v // 2:
-        return False
-    scale = sum_v // 2 + 1
-    dp = [[False] * scale for _ in range(len(nums))]
-
-    nums = sorted(nums, reverse=True)
-    dp[0][nums[0]] = True
-    for i in range(1, len(nums)):
-        dp[i][nums[i]] = True
-        for j in range(scale - 1, 0, -1):
-            if dp[i - 1][j]:
-                dp[i][j] = True
-            if j > nums[i] and dp[i - 1][j - nums[i]]:
-                dp[i][j] = True
-            if dp[i][-1]:
-                return True
-    return dp[-1][-1]
+from tools import *
 
 
-def solution(nums):
+class Solution(object):
+    @print_
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        sum_ = sum(nums)
+        if sum_ % 2 == 1:
+            return False
 
-    def recursive(nums, target):
-        for i, num in enumerate(nums):
-            if num > target:
-                return False
-            elif num == target:
-                return True
-            elif recursive(nums[i + 1:], target - nums[i]):
-                return True
-        return False
+        nums = sorted(nums)
+        memo = {}
 
-    sum_v = sum(nums)
-    if sum_v % 2 == 1:
-        return False
-    nums = sorted(nums, reverse=True)
-    return recursive(nums, sum_v // 2)
+        def dfs(nums, target):
+            for i, num in enumerate(target):
+                sub_ = target - num
+                if sub_ == 0:
+                    return True
+                elif sub_ < 0:
+                elif dfs(, sub_):
+                    # nums[i + 1:] means
+                    return True
+            return False
 
+        return dfs(nums, sum_ // 2)
 
-# print(solution([1, 5, 11, 5]))
-# print(solution([1, 2, 3, 5]))
-# print(solution([1, 2, 5]))
-print(solution([2, 2, 3, 5]))
+    @print_
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        sum_ = sum(nums)
+        if sum_ % 2 == 1:
+            return False
+        sum_v = sum(nums)
+        if sum_v % 2 == 1:
+            return False
+        if max(nums) > sum_v // 2:
+            return False
+        scale = sum_v // 2 + 1
+        dp = [[False] * scale for _ in range(len(nums))]
+
+        nums = sorted(nums, reverse=True)
+        dp[0][nums[0]] = True
+        for i in range(1, len(nums)):
+            dp[i][nums[i]] = True
+            for j in range(scale - 1, 0, -1):
+                if dp[i - 1][j]:
+                    dp[i][j] = True
+                if j > nums[i] and dp[i - 1][j - nums[i]]:
+                    dp[i][j] = True
+                if dp[i][-1]:
+                    return True
+        return dp[-1][-1]
--- problems/test.py
+++ problems/test.py
@@ -12,30 +12,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums = sorted(nums)
-        n = len(nums)
-        ret = []
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            j, k = i + 1, n - 1
-            while j < k:
-                sum_ = nums[i] + nums[j] + nums[k]
-                if sum_ == 0:
-                    ret.append([nums[i], nums[j], nums[k]])
-                    while j < k and nums[j] == nums[j + 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k - 1]:
-                        k -= 1
-                    j += 1
-                    k -= 1
-                elif sum_ < 0:
-                    j += 1
-                else:
-                    k -= 1
-
-        return ret
-
+        
 
 solution = Solution().threeSum
 
