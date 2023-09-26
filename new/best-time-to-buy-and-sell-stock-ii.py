919c66ac69241d0fa692bbeb031311a1963e1609
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-09-26 16:25:25 +0800

    commit

--- new/best-time-to-buy-and-sell-stock-ii.py
+++ new/best-time-to-buy-and-sell-stock-ii.py
@@ -10,8 +10,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        n = len(prices)
+        buy, sell = [0] * n, [0] * n
+        buy[0] = -prices[0]
+        for i in range(1, n):
+            buy[i] = max(buy[i - 1], sell[i - 1] - prices[i])
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i])
+        print(buy, sell)
+        return max(buy[-1], sell[-1])
 
 
 solution = Solution().maxProfit
 
 solution([7, 1, 5, 3, 6, 4])
+solution([7])
