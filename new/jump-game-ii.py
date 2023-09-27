008a3e721a2701d18fb0acd7cadbae0055c211e5
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-09-27 12:01:45 +0800

    commit

--- new/jump-game-ii.py
+++ new/jump-game-ii.py
@@ -5,13 +5,19 @@ from tools import *
 
 class Solution(object):
     @print_
-    class Solution(object):
-        def jump(self, nums):
-            """
-            :type nums: List[int]
-            :rtype: int
-            """
-            
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        last_max = new_max = max_s = 0
+        for i, s in enumerate(nums):
+            if i > last_max:
+                last_max = new_max
+                max_s += 1
+            new_max = max(new_max, i + s)
+        return max_s
+
 
 solution = Solution().jump
 solution([2, 3, 1, 1, 4])
