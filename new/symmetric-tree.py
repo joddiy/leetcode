f85adc920805e2106a1053b91f1c6c0be7417be7
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-01 22:30:34 +0800

    commit

---
+++ new/symmetric-tree.py
@@ -0,0 +1,16 @@
+import math
+import pprint
+from tools import *
+from collections import defaultdict
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+
+solution = Solution().isSymmetric
+print(solution(stringToTreeNode("[1,2,2,3,4,4,3]")))
