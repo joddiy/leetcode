52f6386f04df83e731ca78ab9f49e2437250a30e
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-01 23:36:31 +0800

    commit

---
+++ new/rotate-list.py
@@ -0,0 +1,19 @@
+import math
+import pprint
+from tools import *
+from collections import defaultdict
+
+
+class Solution(object):
+    @print_
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        
+
+
+solution = Solution().rotateRight
+solution(stringToListNode("[1,2,3,4,5]"), k=2)
