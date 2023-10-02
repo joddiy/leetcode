d7dde16047b5ead66637197f7c7a4a360a7a4af2
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-02 10:54:40 +0800

    commit

--- new/3sum.py
+++ new/3sum.py
@@ -12,18 +12,17 @@ class Solution(object):
         """
         nums = sorted(nums)
         nums_map = defaultdict(int)
+        visited = defaultdict(set)
         for i, k in enumerate(nums):
             nums_map[k] = i
         ret = []
-        last_i, last_j = None, None
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 left_ = 0 - (nums[i] + nums[j])
                 k = nums_map[left_]
-                if k > i and k > j and (last_i != nums[i] or last_j != nums[j]):
+                if k > i and k > j and nums[j] not in visited[nums[i]]:
                     ret.append([nums[i], nums[j], left_])
-                    last_i = nums[i]
-                    last_j = nums[j]
+                    visited[nums[i]].add(nums[j])
         return ret
 
 
