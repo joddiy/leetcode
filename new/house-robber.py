46ec72520577c97bd21ce12bf89f67241cc4efe9
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-06 17:56:17 +0800

    commit

---
+++ new/house-robber.py
@@ -0,0 +1,28 @@
+import math
+import pprint
+import sys
+
+from tools import *
+from collections import defaultdict
+
+import heapq
+
+
+class Solution(object):
+    @print_
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        pre = [nums[0], 0]  # rob current, not rob current
+        for i in range(1, len(nums)):
+            pre = [pre[1] + nums[i], max(pre)]
+        return max(pre)
+
+
+solution = Solution().rob
+solution(nums=[1, 2, 3, 1])
