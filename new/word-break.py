a2a400cb2ea5cc25bcc7bee018cc19fc680b50fb
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-06 18:36:51 +0800

    commit

---
+++ new/word-break.py
@@ -0,0 +1,23 @@
+import math
+import pprint
+import sys
+
+from tools import *
+from collections import defaultdict
+
+import heapq
+
+
+class Solution(object):
+    @print_
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+
+
+
+solution = Solution().wordBreak
+solution(s="leetcode", wordDict=["leet", "code"])
