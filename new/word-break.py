ac84fa7fa66742ae813992707fe4ff392a91f742
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-09 17:44:01 +0800

    commit

--- new/word-break.py
+++ new/word-break.py
@@ -16,8 +16,19 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            for w in wordDict:
+                lw = len(w)
+                # get a valid word of s
+                if i >= lw - 1 and s[i - (lw - 1):i + 1] == w:
+                    # previous sub-string is valid
+                    if i - (lw - 1) - 1 < 0 or dp[i - (lw - 1) - 1]:
+                        dp[i] = True
+        return dp[-1]
 
 
 solution = Solution().wordBreak
 solution(s="leetcode", wordDict=["leet", "code"])
+solution(s="applepenapple", wordDict=["apple", "pen"])
+solution(s="catsandog", wordDict=["cats", "dog", "sand", "and", "cat"])
