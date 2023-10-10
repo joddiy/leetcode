e95dea9fea1fa70b9f2e1690a0e34925704f8ee6
Zhaoqi Zhang <joddiyzhang@gmail.com>
2023-10-10 14:52:23 +0800

    commit

---
+++ new/minimum-window-substring.py
@@ -0,0 +1,50 @@
+import math
+import pprint
+import sys
+
+from tools import *
+from collections import *
+
+import heapq
+
+
+class Solution(object):
+    @print_
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        n = len(s)
+        count = len(t)
+        count_w = Counter(t)
+        b, l = 0, sys.maxsize
+        i, j = 0, 0
+        while j < n:
+            # extend the right side
+            if count_w[s[j]] > 0:
+                count -= 1
+            count_w[s[j]] -= 1
+
+            # shrink the left side
+            while count == 0:
+                # update the max length
+                if (j - i + 1) < l:
+                    l = j - i + 1
+                    b = i
+                count_w[s[i]] += 1
+                if count_w[s[i]] > 0:
+                    count += 1
+                i += 1
+            j += 1
+        if l < sys.maxsize:
+            return s[b:b + l]
+        else:
+            return ""
+
+
+solution = Solution().minWindow
+solution(s="ADOBECODEBANC", t="ABC")
+# solution(s="a", t="a")
+# solution(s="a", t="aa")
