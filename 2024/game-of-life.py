132a0060be2f1a3e712d1a833bd8983f4885eef2
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-05-28 12:02:42 +0800

    commit

--- 2024/game-of-life.py
+++ 2024/game-of-life.py
@@ -12,8 +12,31 @@ class Solution(object):
         :type board: List[List[int]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        
+        # 1, < 2, die
+        # 1, = 2 | 3, live
+        # 1, > 3, die
+        # 0, = 3, live
+        neighbours = [(-1, -1), (-1, 0), (0, -1), (0, 1), (1, 0), (1, 1), (-1, 1), (1, -1)]
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                live = 0
+                for x, y in neighbours:
+                    if 0 <= i + x < m and 0 <= j + y < n and board[i + x][j + y] >= 1:
+                        live += 1
+                if board[i][j] == 0 and live == 3:
+                    board[i][j] = -1  # 0 -> 1
+                elif board[i][j] == 1 and (live < 2 or live > 3):
+                    board[i][j] = 2  # 1 -> 0
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 2:
+                    board[i][j] = 0
+                if board[i][j] == -1:
+                    board[i][j] = 1
+        return board
 
 
 solution = Solution().gameOfLife
 solution(board=[[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]])
+solution(board=[[1, 1], [1, 0]])
