aeaaf6932ec88e80f2b3ef591c5ed742327d94cb
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-05-30 13:26:09 +0800

    commit

--- 2024/number-of-islands.py
+++ 2024/number-of-islands.py
@@ -13,21 +13,39 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        pos = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        # pos = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        # m, n = len(grid), len(grid[0])
+        # ret = 0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] == '1':
+        #             array = [(i, j)]
+        #             while array:
+        #                 i_, j_ = array.pop(0)
+        #                 if 0 <= i_ < m and 0 <= j_ < n:
+        #                     if grid[i_][j_] == '1':
+        #                         for x, y in pos:
+        #                             array.append((i_ + x, j_ + y))
+        #                     grid[i_][j_] = '0'
+        #             ret += 1
+        # return ret
         m, n = len(grid), len(grid[0])
+        self.pos = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+
+        def remove(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == "0":
+                return
+            else:
+                grid[i][j] = "0"
+                for x, y in self.pos:
+                    remove(i + x, j + y)
+
         ret = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    array = [(i, j)]
-                    while array:
-                        i_, j_ = array.pop(0)
-                        if 0 <= i_ < m and 0 <= j_ < n:
-                            if grid[i_][j_] == '1':
-                                for x, y in pos:
-                                    array.append((i_ + x, j_ + y))
-                            grid[i_][j_] = '0'
+                if grid[i][j] == "1":
                     ret += 1
+                    remove(i, j)
         return ret
 
 
