147248b7848d9ea9b8d44a3850b3064abe00780e
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-01 17:08:12 +0800

    commit

---
+++ 2024/maximal-square.py
@@ -0,0 +1,33 @@
+import math
+import sys
+
+from tools import *
+import pprint
+from collections import defaultdict
+
+
+class Solution(object):
+    @print_
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        max_l = 0
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = int(matrix[i][j])
+                elif matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                max_l = max(max_l, dp[i][j])
+        return max_l * max_l
+
+
+solution = Solution().maximalSquare
+solution(
+    matrix=[["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]])
+solution(matrix=[["0", "1"], ["1", "0"]])
+solution(matrix=[["0"]])
