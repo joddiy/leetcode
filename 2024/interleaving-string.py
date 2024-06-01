7484989a61d4397949fd1fb7afb51afe3ac6e426
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-01 16:34:58 +0800

    commit

---
+++ 2024/interleaving-string.py
@@ -0,0 +1,42 @@
+import math
+import sys
+
+from tools import *
+import pprint
+from collections import defaultdict
+
+
+class Solution(object):
+    @print_
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        m, n = len(s1), len(s2)
+        if m + n != len(s3):
+            return False
+        if m == 0:
+            return s2 == s3
+        if n == 0:
+            return s1 == s3
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                    continue
+                if s1[i - 1] == s3[i + j - 1] and dp[i - 1][j]:
+                    dp[i][j] = True
+                if s2[j - 1] == s3[i + j - 1] and dp[i][j - 1]:
+                    dp[i][j] = True
+        return dp[-1][-1]
+
+
+solution = Solution().isInterleave
+solution(s1="aabcc", s2="dbbca", s3="aadbbcbcac")
+solution(s1="aabcc", s2="dbbca", s3="aadbbbaccc")
+solution(s1="", s2="", s3="")
