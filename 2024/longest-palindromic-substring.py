6359197fc71067c2198290538dfe7a0ba6479ad8
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-01 16:02:27 +0800

    commit

---
+++ 2024/longest-palindromic-substring.py
@@ -0,0 +1,34 @@
+import math
+import sys
+
+from tools import *
+import pprint
+from collections import defaultdict
+
+
+class Solution(object):
+    @print_
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ret = ""
+        for i in range(n):  # length -1
+            for j in range(n - i):  # start_idx
+                if i == 0:
+                    dp[i][j] = True
+                elif i == 1:
+                    dp[i][j] = (s[j] == s[j + 1])
+                elif s[j] == s[j + i] and dp[i - 2][j + 1]:
+                    dp[i][j] = True
+                if dp[i][j] and (i + 1) > len(ret):
+                    ret = s[j:j + i + 1]
+        return ret
+
+
+solution = Solution().longestPalindrome
+solution(s="babad")
+solution(s="cbbd")
---
+++ 2024/minimum-path-sum.py
@@ -0,0 +1,32 @@
+import math
+import sys
+
+from tools import *
+import pprint
+from collections import defaultdict
+
+
+class Solution(object):
+    @print_
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[0][0] = grid[0][0]
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[-1][-1]
+
+
+solution = Solution().minPathSum
+solution(grid=[[1, 3, 1], [1, 5, 1], [4, 2, 1]])
