e06ada9ee20ab5e3e4e71cce36780481dd44962b
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-02 15:42:27 +0800

    commit

--- 2024/maximum-subarray.py
+++ 2024/maximum-subarray.py
@@ -13,9 +13,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
+        j = 0
+        ret = -sys.maxsize
+        cur = 0
+        while j < n:
+            cur += nums[j]
+            ret = max(ret, cur)
+            if cur <= 0:
+                cur = 0
+            j += 1
+        return ret
 
 
 solution = Solution().maxSubArray
 solution(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4])
 solution(nums=[1])
+solution(nums=[-1])
+solution(nums=[0])
 solution(nums=[5, 4, -1, 7, 8])
--- 2024/maximum-sum-circular-subarray.py
+++ 2024/maximum-sum-circular-subarray.py
@@ -13,6 +13,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
+
+        j = 0
+        max_ = -sys.maxsize
+        cur = 0
+        while j < n:
+            cur += nums[j]
+            max_ = max(max_, cur)
+            if cur <= 0:
+                cur = 0
+            j += 1
+
+        j = 0
+        min_ = sys.maxsize
+        cur = 0
+        while j < n:
+            cur += nums[j]
+            min_ = min(min_, cur)
+            if cur >= 0:
+                cur = 0
+            j += 1
+
+        if max_ < 0:
+            return max_
+        else:
+            return max(max_, sum(nums) - min_)
 
 
 solution = Solution().maxSubarraySumCircular
