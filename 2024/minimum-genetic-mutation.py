383009f8bc495adfb8e40456b139540074a72004
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-02 14:38:18 +0800

    commit

--- 2024/minimum-genetic-mutation.py
+++ 2024/minimum-genetic-mutation.py
@@ -8,13 +8,38 @@ from collections import defaultdict
 
 class Solution(object):
     @print_
-    def snakesAndLadders(self, board):
+    def minMutation(self, startGene, endGene, bank):
         """
-        :type board: List[List[int]]
+        :type startGene: str
+        :type endGene: str
+        :type bank: List[str]
         :rtype: int
         """
+        n = len(bank)
+        all_genes = [startGene] + bank
+        distance_map = defaultdict(list)
+        for i in range(len(all_genes)):
+            for j in range(1, len(all_genes)):
+                a, b = all_genes[i], all_genes[j]
+                sum_ = sum(1 for x in range(len(a)) if a[x] != b[x])
+                if sum_ == 1:
+                    distance_map[a].append(b)
+                    distance_map[b].append(a)
+
+        array = [(startGene, 0)]
+        visited = set(startGene)
+        while array:
+            gene, path = array.pop(0)
+            if gene == endGene:
+                return path
+            else:
+                for next_gene in distance_map[gene]:
+                    if next_gene not in visited:
+                        array.append((next_gene, path + 1))
+                        visited.add(next_gene)
+        return -1
 
 
-solution = Solution().snakesAndLadders
+solution = Solution().minMutation
 solution(startGene="AACCGGTT", endGene="AACCGGTA", bank=["AACCGGTA"])
 solution(startGene="AACCGGTT", endGene="AAACGGTA", bank=["AACCGGTA", "AACCGCTA", "AAACGGTA"])
