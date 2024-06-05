f6c1cb4939e3a51a5bd2972789c14e18f403d4e1
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-05 23:39:25 +0800

    commit

--- new/word-search-ii.py
+++ new/word-search-ii.py
@@ -8,46 +8,55 @@ from collections import defaultdict
 import heapq
 
 
-class TrieNode:
+class TrieNode(object):
     def __init__(self):
         self.children = defaultdict(TrieNode)
-        self.isWord = False
+        self.is_word = False
 
-    def addWord(self, word):
+    def add_word(self, word):
         cur = self
-        for ch in word:
-            cur = cur.children[ch]
-        cur.isWord = True
+        for c in word:
+            cur = cur.children[c]
+        cur.is_word = True
 
 
 class Solution:
+    @print_
     def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
         root = TrieNode()
         for word in words:
-            root.addWord(word)
+            root.add_word(word)
 
         m, n = len(board), len(board[0])
-        res = set()
+        ret = set()
 
-        def dfs(i, j, node=root, word=""):
-            if i < 0 or i == m or j < 0 or j == n or board[i][j] not in node.children:
+        def dfs(i, j, node, prefix):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] not in node.children:
                 return
+
             node = node.children[board[i][j]]
-            word += board[i][j]
-            if node.isWord:
-                res.add(word)
+            prefix += board[i][j]
+            if node.is_word:
+                ret.add(prefix)
 
             board[i][j] = '#'
-            dfs(i + 1, j, node, word)
-            dfs(i - 1, j, node, word)
-            dfs(i, j + 1, node, word)
-            dfs(i, j - 1, node, word)
-            board[i][j] = word[-1]
+            dfs(i - 1, j, node, prefix)
+            dfs(i + 1, j, node, prefix)
+            dfs(i, j - 1, node, prefix)
+            dfs(i, j + 1, node, prefix)
+            board[i][j] = prefix[-1]
+            return
 
         for i in range(m):
             for j in range(n):
-                dfs(i, j)
-        return list(res)
+                dfs(i, j, root, "")
+
+        return list(ret)
 
 
 solution = Solution().findWords
@@ -57,3 +66,4 @@ solution(board=[
     ["i", "h", "k", "r"],
     ["i", "f", "l", "v"]
 ], words=["oath", "pea", "eat", "rain"])
+solution(board=[["a", "b"], ["c", "d"]], words=["abcb"])
