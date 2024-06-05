60ba6c35c5334b3646a8433f0868d80efa51e408
zhaoqi.zhang <zhaoqi.zhang@shopee.com>
2024-06-05 16:39:19 +0800

    commit

--- new/basic-calculator.py
+++ new/basic-calculator.py
@@ -17,42 +17,41 @@ class Solution(object):
         """
         stack = []
         result = 0
-        number = 0
         sign = 1
+        number = 0
 
         for c in s:
             if c.isdigit():
-                number = 10 * number + int(c)
-            elif c == '+':
-                result += sign * number
+                number = number * 10 + int(c)
+            else:
+                if c == '+':
+                    result += sign * number
+                    sign = 1
+                elif c == '-':
+                    result += sign * number
+                    sign = -1
+                elif c == '(':
+                    stack.append(result)
+                    stack.append(sign)
+                    result = 0
+                    sign = 1
+                elif c == ')':
+                    result += sign * number
+                    result *= stack.pop()
+                    result += stack.pop()
+                else:
+                    continue
                 number = 0
-                sign = 1
-            elif c == '-':
-                result += sign * number
-                number = 0
-                sign = -1
-            elif c == '(':
-                stack.append(result)
-                stack.append(sign)
-                sign = 1
-                result = 0
-            elif c == ')':
-                result += sign * number
-                number = 0
-                result *= stack.pop()  # Pop the sign before the parenthesis
-                result += stack.pop()  # Pop the result calculated before the parenthesis
-
-        if number != 0:
+        if number > 0:
             result += sign * number
-
         return result
 
 
 solution = Solution().calculate
-solution(s="1 + 1")
-solution(s=" 2-1 + 2 ")
-solution(s="(1+(4+5+2)-3)+(6+8)")
-solution(s="(1)")
-solution(s="2147483647")
+# solution(s="1 + 1")
+# solution(s=" 2-1 + 2 ")
+# solution(s="(1+(4+5+2)-3)+(6+8)")
+# solution(s="(1)")
+# solution(s="2147483647")
 solution(s="1-(     -2)")
-solution(s="- (3 + (4 + 5))")
+# solution(s="- (3 + (4 + 5))")
